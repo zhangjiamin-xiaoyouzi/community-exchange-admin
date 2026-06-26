3b84ddc9b3b3e9cb107f850038ddcf22eaca696b
jamie <jamiechang1001@gmail.com>
2026-06-26 10:11:14 +0800

    chore: add GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/community-exchange-admin/',
   plugins: [vue()],
   server: { port: 5181, host: true }
 })
